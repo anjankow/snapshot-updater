4b1d4d0fa90f3701d643ae45ab0e8add72a0413e
anjankow <anjankow@protonmail.com>
2023-03-08 20:59:16 +0100

    getGoPath

--- updateFileSnapshots.py
+++ updateFileSnapshots.py
@@ -1,25 +1,63 @@
 import re
 from typing import List
+import os
 
 def findTestNames(filePath: str) ->List[str]:
-    f = open(filePath, "r")
+    f = open(filePath, 'r')
     # first let's collect names of the tests
     testNames = []
     for line in f:
         # \w matches word character: (characters from a to Z, digits from 0-9, and the underscore _ character)
         # brackets () capture a group
-        testRegex = "^func (\w+).*testing.T"
+        testRegex = '^func (\w+).*testing.T'
         matched = re.search(testRegex, line)
 
         if matched is None:
             continue
 
-        testName = matched.group(1)
-        testNames.append(testName)
+        name = matched.group(1)
+        testNames.append(name)
 
     return testNames
 
+def runTests(packagePath: str, testNames: List[str]):
+    # to run all tests from a single file:
+    #   go test -timeout 30s -run ^(TestName1|TestName2)$ current/package/path
+    
+    
+    pass
+
+# takes go path from PATH
+def getGoPath()-> str:
+    PATH = os.getenv('PATH')
+    matchedIdx = PATH.find('go/bin')
+    if matchedIdx < 0:
+        print('Error: can\'t find go executable in the PATH')
+        return ''
+
+    delimiter = ':'
+    startIdx = PATH.rfind(delimiter, 0, matchedIdx)
+    if startIdx < 0:
+        startIdx = 0
+    else:
+        # path will start with ':' otherwise
+        startIdx += 1
+
+    endIdx = PATH.find(delimiter, startIdx+1)
+    if endIdx < 0:
+        endIdx = len(PATH)
+
+    goPath = PATH[startIdx:endIdx]
+    return goPath
+
 
 filePath = "??"
+packagePath = "??"
 
-print(findTestNames(filePath))
+testNames = findTestNames(filePath)
+if len(testNames) == 0:
+    exit(0)
+
+print(getGoPath())
+
+# runTests(packagePath, testNames)
