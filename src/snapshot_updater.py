0d469508c94910faf711c69069e79840dd181711
anjankow <anjankow@protonmail.com>
2023-05-09 14:09:59 +0000

    use TEST_UPDATE_GOLDEN instead of regex replace

--- src/snapshot_updater.py
+++ src/snapshot_updater.py
@@ -6,8 +6,7 @@ import subprocess
 
 # Updates snapshots in the given package.
 # Works only for snapshots created by https://github.com/allaboutapps/go-starter/blob/master/internal/test/helper_snapshot.go
-# Changes the snapshotter's `Save` function to `SaveU` and runs the tests to update corresponding snapshots.
-# Then changes it back to `Save` and runs the tests again.
+# Runs the tests with TEST_UPDATE_GOLDEN=true. Then runs the tests again without updating the snapshots.
 # projectBaseDir: absolute directory of the project
 # packageDir: absolute directory of the package
 # filePath: when given, only snapshots of the tests from this file will be updated. Should be an absolute path.
@@ -38,30 +37,23 @@ def update(projectBaseDir: str, packageDir: str, filePath: str='', verbose: bool
     modulePackagePath = _getModulePackagePath(projectBaseDir, relativePkgPath)
 
     print('Updating snapshots...')
-    # set snaphotter mode to UPDATE (SaveU)
-    _setSnapshotterMode(True, packageDir, filePath)
 
     # run tests to update the snapshots
-    _runTests(goPath, projectBaseDir, modulePackagePath, filePath, verbose)
+    _runTests(goPath, projectBaseDir, modulePackagePath, filePath,updateSnaphots=True, verbose=verbose)
     # tests should fail because of updated snapshots
-
-    print()
-    print('Reverting test files changes...')
-    # now unset UPDATE mode (set back to Save)
-    _setSnapshotterMode(False, packageDir, filePath)
 
     print()
     print('Running the tests again...')
     # and run the tests again
-    _runTests(goPath, projectBaseDir, modulePackagePath, filePath, verbose)
+    _runTests(goPath, projectBaseDir, modulePackagePath, filePath,  updateSnaphots=False, verbose=verbose)
     # all should pass now
 
 
-def _runTests(goPath: str, projectBasePath: str, modulePackagePath: str, filePath: str, verbose=True):
+def _runTests(goPath: str, projectBasePath: str, modulePackagePath: str, filePath: str, updateSnaphots: bool, verbose=True):
     if filePath == '':
-        _runPackageTests(goPath, projectBasePath, modulePackagePath, verbose)
+        _runPackageTests(goPath, projectBasePath, modulePackagePath, updateSnaphots, verbose)
     else:
-        _runFileTests(goPath, projectBasePath, modulePackagePath, filePath, verbose)
+        _runFileTests(goPath, projectBasePath, modulePackagePath, filePath, updateSnaphots, verbose)
 
 
 # Finds all test names in a given file
@@ -113,20 +105,22 @@ def _getGoModuleName(projectBasePath: str) -> str:
 # projectBasePath: absolute directory of the go.mod file
 # modulePackagePath: package path relative to the projectBasePath prefixed with go module name
 # filePath: absolute path of the file
-def _runFileTests(goPath: str, projectBasePath: str, modulePackagePath: str, filePath: str, verbose=True):
+def _runFileTests(goPath: str, projectBasePath: str, modulePackagePath: str, filePath: str,updateSnaphots: bool, verbose=True):
     # get test names from the file
     testNames = _findTestNames(filePath)
     testNamesStr = ""
     for name in testNames:
         testNamesStr+=name + '|'
     testNamesStr= testNamesStr.rstrip('|')
-    
+
     # prepare command
     command = [f"{goPath}", "test","-v", "-run", f"^({testNamesStr})$", f"{modulePackagePath}"]
     if not verbose:
          command.remove("-v")
 
-    p = subprocess.Popen(command, cwd=projectBasePath)
+    p = subprocess.Popen(command,
+                         cwd=projectBasePath,
+                         env={**os.environ, 'TEST_UPDATE_GOLDEN': str(updateSnaphots).lower()})
     p.wait()
 
 
@@ -136,12 +130,14 @@ def _runFileTests(goPath: str, projectBasePath: str, modulePackagePath: str, fil
 # goPath: path to go executable
 # projectBasePath: absolute directory of the go.mod file
 # modulePackagePath: package path relative to the projectBasePath prefixed with go module name
-def _runPackageTests(goPath: str, projectBasePath: str, modulePackagePath: str, verbose=True):
+def _runPackageTests(goPath: str, projectBasePath: str, modulePackagePath: str, updateSnaphots: bool, verbose=True):
     command = [f"{goPath}", "test","-v", f"{modulePackagePath}"]
     if not verbose:
          command.remove("-v")
 
-    p = subprocess.Popen(command, cwd=projectBasePath)
+    p = subprocess.Popen(command,
+                         cwd=projectBasePath,
+                         env={**os.environ, 'TEST_UPDATE_GOLDEN': str(updateSnaphots).lower()})
     p.wait()
 
 
@@ -179,49 +175,3 @@ def _getGoPath()-> str:
         # path doesn't exist? continue checking other PATH entries
         searchStartIdx = matchedIdx + 1
         continue
-
-
-# Snapshotter https://github.com/allaboutapps/go-starter/blob/master/internal/test/helper_snapshot.go
-# In the test code `shapshotter.Save(t, resopose)` saves the response snapshot or, if snaphot exists already, compares it.
-# Changing it to `snapshotter.SaveU(t, response)` updates the snapshot with the new response data and fails the test.
-#
-# If update param is true, this function finds all occurences of `.Save(t, [\w0-9]+)` and replaces it with `SaveU`.
-# If set to false, `SaveU` is replaced by `Save`.
-# The occurences are found within the given file or in the entire package, if file path is not empty.
-def _setSnapshotterMode(update: bool, absPackagePath: str, filePath=''):
-    files = []
-    if filePath == '':
-        for file in glob.glob(os.path.join(absPackagePath, '*_test.go')):
-            print(file)
-            files.append(file)
-    else:
-        files = [filePath]
-
-    if len(files) == 0:
-        print('No go test files found')
-        exit(1)
-
-    variants = ['Save', 'SaveU']
-    if update:
-        replacementVariant = 1
-    else:
-        replacementVariant = 0
-
-    # \.Save\(t, [\w0-9]+\) or \.SaveU\(t, [\w0-9]+\)
-    pattern = f'\.{variants[abs(replacementVariant-1)]}\(t, [\w0-9]+\)'
-
-    for file in files:
-        print('Processing file: ', file)
-        content = []
-        with open(file, 'r') as f:
-            content = str(f.read()).splitlines()
-
-        with open(file, 'w')as f:
-            f.truncate()
-            for line in content:
-                x = re.search(pattern, line)
-                if x is not None:
-                    after = f'.{variants[replacementVariant]}('
-                    before = f'.{variants[abs(replacementVariant-1)]}('
-                    line = line.replace(before, after )
-                f.write(line + '\n')
